37dc6745e79b151afed336cbd944d6b0ece0ec94
adri-23 <adrianabermudez1023@gmail.com>
2021-07-11 19:00:13 -0500

    operadores logicos

---
+++ OperadoresLogicos/src/OperadoresLogicos.java
@@ -0,0 +1,12 @@
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(saludo());
+	}
+ public static String saludo() {
+	 return "saludo";
+ }
+}
